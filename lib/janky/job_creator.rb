38bded3e81d45684166cebb8dc01cb5c9c08e0a7
Vivek Pandey <vivek.pandey@gmail.com>
2011-12-20 19:42:23 -0800

    Fixed typo

--- lib/janky/job_creator.rb
+++ lib/janky/job_creator.rb
@@ -72,7 +72,7 @@ module Janky
         user = uri.user
         pass = uri.password
         http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl=true if (uri.scheme.casecmp"https" == 0)
+        http.use_ssl=true if (uri.scheme.casecmp("https") == 0)
         get = Net::HTTP::Get.new("/job/#{name}/")
         get.basic_auth(user, pass) if user && pass
         response = http.request(get)
