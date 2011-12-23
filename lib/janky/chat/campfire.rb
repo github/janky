463c9e004c8269babcb69015d4b0049250d4af8d
Seth Chisamore <schisamo@opscode.com>
2011-12-23 12:07:11 -0500

    remove frivolous Broach class and adapter attribute from Campfire adapter

--- lib/janky/chat/campfire.rb
+++ lib/janky/chat/campfire.rb
@@ -13,11 +13,6 @@ module Janky
           "token"   => settings['JANKY_CAMPFIRE_TOKEN'],
           "use_ssl" => true
         }
-        @adapter = Broach.new
-      end
-
-      class << self
-        attr_accessor :adapter
       end
 
       # Send a message to a Campfire room.
@@ -27,27 +22,15 @@ module Janky
       #
       # Returns nothing.
       def self.speak(message, room_id, output={})
-        adapter.speak(room_name(room_id), message)
+        ::Broach.speak(room_name(room_id), message)
       end
 
       # Memoized list of available rooms.
       #
       # Returns an Array of Broach::Room objects.
       def self.rooms
-        @rooms ||= adapter.rooms
+        @rooms ||= ::Broach.rooms
       end
-
-      class Broach
-        def speak(room_name, message)
-          ::Broach.speak(room_name, message)
-        end
-
-        def rooms
-          ::Broach.rooms
-        end
-      end
-
-
     end
   end
 end
