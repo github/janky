20aeed880f7562b3bda7f6bfe9fbe2a46e5ba891
Seth Chisamore <schisamo@opscode.com>
2011-12-23 14:47:11 -0500

    move Janky::Chat::Mock implementation into it's own file, following idioms throughout the rest of the project.

--- lib/janky/chat.rb
+++ lib/janky/chat.rb
@@ -89,27 +89,5 @@ module Janky
     def self.rooms=(value)
       service.rooms = value
     end
-
-    class Mock
-      def initialize
-        @rooms = {}
-      end
-
-      attr_writer :rooms
-
-      def speak(room_name, message)
-        if !@rooms.values.include?(room_name)
-          raise Error, "Unknown room #{room_name.inspect}"
-        end
-      end
-
-      def rooms
-        acc = []
-        @rooms.each do |id, name|
-          acc << Room.new(id, name)
-        end
-        acc
-      end
-    end
   end
 end
---
+++ lib/janky/chat/mock.rb
@@ -0,0 +1,26 @@
+module Janky
+  module Chat
+    # Mock chat implementation used in testing environments.
+    class Mock
+      def initialize
+        @rooms = {}
+      end
+
+      attr_writer :rooms
+
+      def speak(room_name, message)
+        if !@rooms.values.include?(room_name)
+          raise Error, "Unknown room #{room_name.inspect}"
+        end
+      end
+
+      def rooms
+        acc = []
+        @rooms.each do |id, name|
+          acc << Room.new(id, name)
+        end
+        acc
+      end
+    end
+  end
+end
