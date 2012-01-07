2da56cba8e5937c5c756367e6716085bc06ad580
Justin Smestad <justin.smestad@gmail.com>
2012-01-06 19:50:33 -0700

    Debug output

--- lib/janky/chat/hipchat.rb
+++ lib/janky/chat/hipchat.rb
@@ -2,6 +2,7 @@ module Janky
   module Chat
     class HipChat
       def initialize(settings)
+        puts "Chat Settings: #{settings}"
         @client = ::HipChat::Client.new(settings["JANKY_CHAT_HIPCHAT_TOKEN"])
         @from = settings["JANKY_CHAT_HIPCHAT_FROM"] || "CI"
       end
@@ -11,6 +12,7 @@ module Janky
       end
 
       def rooms
+        puts "Rooms: #{@client.rooms}" rescue nil
         @rooms ||= @client.rooms.map do |room|
           Room.new(room.room_id, room.name)
         end
