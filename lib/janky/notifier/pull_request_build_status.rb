27e4e08f58856d103aab03a2b2a6ef23c3ddf04b
Faraaz Ismail <fism88@gmail.com>
2012-08-16 14:53:03 -0500

    not using GitHub module?

--- lib/janky/notifier/pull_request_build_status.rb
+++ lib/janky/notifier/pull_request_build_status.rb
@@ -1,15 +1,13 @@
 module Janky
   module Notifier
-    module GitHub
-      class PullRequestBuildStatus
-        def self.completed(build)
-          pr_number = Janky::GitHub.get_pull_request_number(build.repo_nwo, build.branch_name)
+    class PullRequestBuildStatus
+      def self.completed(build)
+        pr_number = Janky::GitHub.get_pull_request_number(build.repo_nwo, build.branch_name)
 
-          if build.green?
-            Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart:")
-          else
-            Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":exclamation:")
-          end
+        if build.green?
+          Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart:")
+        else
+          Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":exclamation:")
         end
       end
     end
