8e74cc2f5f25e36f5739f2275068138e3579a58f
Faraaz Ismail <fism88@gmail.com>
2012-08-16 14:50:55 -0500

    Fix indentation

--- lib/janky/notifier/pull_request_build_status.rb
+++ lib/janky/notifier/pull_request_build_status.rb
@@ -4,11 +4,12 @@ module Janky
       class PullRequestBuildStatus
         def self.completed(build)
           pr_number = Janky::GitHub.get_pull_request_number(build.repo_nwo, build.branch_name)
-         if build.green?
-           Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart:")
-         else
-           Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":exclamation:")
-         end
+
+          if build.green?
+            Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart:")
+          else
+            Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":exclamation:")
+          end
         end
       end
     end
