4d569e47ee822203fea8976953502aba238901b6
Faraaz Ismail <fism88@gmail.com>
2012-08-16 15:07:00 -0500

    use absolute path?

--- lib/janky/notifier/pull_request_build_status.rb
+++ lib/janky/notifier/pull_request_build_status.rb
@@ -5,9 +5,9 @@ module Janky
         pr_number = Janky::GitHub.get_pull_request_number(build.repo_nwo, build.branch_name)
 
         if build.green?
-          Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart:")
+          ::Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart:")
         else
-          Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":exclamation:")
+          ::Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":exclamation:")
         end
       end
     end
