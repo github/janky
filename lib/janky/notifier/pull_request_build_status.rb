51467ce188a0dbd63830fdfcd711e9a66cbf1872
Faraaz Ismail <fism88@gmail.com>
2012-08-20 18:02:38 -0500

    Add @ between image and sha

--- lib/janky/notifier/pull_request_build_status.rb
+++ lib/janky/notifier/pull_request_build_status.rb
@@ -4,9 +4,9 @@ module Janky
       def self.completed(build)
         if (pr_number = ::Janky::GitHub.get_pull_request_number(build.repo_nwo, build.branch_name))
           if build.green?
-            ::Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart: #{build.sha1}")
+            ::Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart: @ #{build.sha1}")
           else
-            ::Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":broken_heart: #{build.sha1}")
+            ::Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":broken_heart: @ #{build.sha1}")
           end
         end
       end
