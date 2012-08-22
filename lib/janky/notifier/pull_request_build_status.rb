d1ce9179adf86e1563c7987c410dd6e7c8170247
Faraaz Ismail <fism88@gmail.com>
2012-08-22 14:20:13 -0500

    Teach Janky how to comment on open Pull Requests

---
+++ lib/janky/notifier/pull_request_build_status.rb
@@ -0,0 +1,15 @@
+module Janky
+  module Notifier
+    class PullRequestBuildStatus
+      def self.completed(build)
+        if (pr_number = ::Janky::GitHub.get_pull_request_number(build.repo_nwo, build.branch_name))
+          if build.green?
+            ::Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":green_heart: @ #{build.sha1}")
+          else
+            ::Janky::GitHub.comment_on_pull_request(build.repo_nwo, pr_number, ":broken_heart: @ [`#{build.short_sha1}`](#{build.web_url})")
+          end
+        end
+      end
+    end
+  end
+end
