cbf6b1193c4ac501f4a51b1a790a935581540a0f
Rob Sanheim <rsanheim@gmail.com>
2012-12-11 14:02:19 -0600

    It helps to use a different name for tests

--- test/repository_test.rb
+++ test/repository_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../test_helper", __FILE__)
 
-class JankyTest < Test::Unit::TestCase
+class RepositoryTest < Test::Unit::TestCase
   def setup
     Janky.setup(environment)
     Janky.enable_mock!
