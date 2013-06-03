9c6fb4aa33cc11f9a21991ed5e79410466b62255
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-06-03 19:15:23 -0300

    A better build number

--- lib/janky/views/index.rb
+++ lib/janky/views/index.rb
@@ -5,7 +5,7 @@ module Janky
       def jobs
         @builds.collect do |build|
           {
-            :build_number    => build.id,
+            :build_number    => "##{build.id}",
             :console_path    => "/#{build.number}/output",
             :compare_url     => build.compare,
             :repo_path       => "/#{build.repo_name}",
