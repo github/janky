f15616dc2959b8cb37b420135b8cc83e7daa2ef6
mbartmann <mathias.bartmann@netflower.de>
2014-05-17 20:25:57 +0200

    Adds attachments to Slack adapter to give the message a nicer touch

--- lib/janky/chat_service/slack.rb
+++ lib/janky/chat_service/slack.rb
@@ -3,6 +3,9 @@ require "slack"
 module Janky
   module ChatService
     class Slack
+      Attachment = Struct.new(:fallback, :text, :pretext, :color, :mrkdwn_in, :fields)
+      AttachmentField = Struct.new(:title, :value, :short)
+
       def initialize(settings)
         team = settings["JANKY_CHAT_SLACK_TEAM"]
 
@@ -20,13 +23,38 @@ module Janky
       end
 
       def speak(message, room_id, options = {})
-        @client.post_message(message, room_id, options)
+        if options[:build].present?
+          @client.post_message(nil, room_id, {attachments: attachments(message, options[:build])})
+        else
+          @client.post_message(message, room_id, options)
+        end
       end
 
       def rooms
         @rooms ||= @client.channels.map do |channel|
           Room.new(channel['id'], channel['name'])
         end
+      end
+
+      private
+
+      def attachments(fallback, build)
+        status = build.green? ? "was successful" : "failed"
+        color = build.green? ? "good" : "danger"
+
+        message = "Build #%s of %s/%s %s" % [
+          build.number,
+          build.repo_name,
+          build.branch_name,
+          status
+        ]
+
+        janky_field = AttachmentField.new("Janky", build.web_url, false)
+        commit_field = AttachmentField.new("Commit", "<#{build.commit_url}|#{build.short_sha1}>", true)
+        duration_field = AttachmentField.new("Duration", "#{build.duration}s", true)
+        fields = [janky_field.to_h, commit_field.to_h, duration_field.to_h]
+
+        [Attachment.new(fallback, message, nil, color, ["text", "title", "fallback"], fields)]
       end
     end
   end
