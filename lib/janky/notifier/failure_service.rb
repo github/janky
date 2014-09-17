5a41c60472e1ef76af51fe38fa6d772fc446afc2
Bob Van Landuyt <bob@vanlanduyt.co>
2014-09-17 10:00:31 +0200

    Don't post in the same room twice

--- lib/janky/notifier/failure_service.rb
+++ lib/janky/notifier/failure_service.rb
@@ -4,6 +4,7 @@ module Janky
       def self.completed(build)
         return if build.green?
         return unless failure_room = ENV["JANKY_CHAT_FAILURE_ROOM"]
+        return if failure_room == build.room_id
         ::Janky::ChatService.speak(message(build), failure_room, {:color => color(build)})
       end
     end
