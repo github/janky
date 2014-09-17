ceaa502d2fa5fe8c606a19a98065871758ae255b
Bob Van Landuyt <bob@vanlanduyt.co>
2014-09-17 15:45:16 +0200

    Combine wether or not to post a failure notification

--- lib/janky/notifier/failure_service.rb
+++ lib/janky/notifier/failure_service.rb
@@ -2,10 +2,16 @@ module Janky
   module Notifier
     class FailureService < ChatService
       def self.completed(build)
-        return if build.green?
-        return unless failure_room = ENV["JANKY_CHAT_FAILURE_ROOM"]
-        return if failure_room == build.room_id
+        return unless need_failure_notification?(build)
         ::Janky::ChatService.speak(message(build), failure_room, {:color => color(build)})
+      end
+
+      def self.failure_room
+        ENV["JANKY_CHAT_FAILURE_ROOM"]
+      end
+
+      def self.need_failure_notification?(build)
+        build.red? && failure_room != build.room_id
       end
     end
   end
