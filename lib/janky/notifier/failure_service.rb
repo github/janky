37d5503f38f3606643e24d7aafd70be7b42c3956
Bob Van Landuyt <bob@vanlanduyt.co>
2014-09-17 10:00:12 +0200

    Fix a typo

--- lib/janky/notifier/failure_service.rb
+++ lib/janky/notifier/failure_service.rb
@@ -3,7 +3,7 @@ module Janky
     class FailureService < ChatService
       def self.completed(build)
         return if build.green?
-        retrun unless failure_room = ENV["JANKY_CHAT_FAILURE_ROOM"]
+        return unless failure_room = ENV["JANKY_CHAT_FAILURE_ROOM"]
         ::Janky::ChatService.speak(message(build), failure_room, {:color => color(build)})
       end
     end
